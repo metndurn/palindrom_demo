24393a7c87333b7e4cd275f28f16a08bea2f5113
metndurn <mtndrn123@gmail.com>
2023-02-04 01:46:06 +0300

    DENEME CALISMASIDIR

---
+++ PalindromOrnegi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromOrnegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            basadon:
+            Console.WriteLine("Lutfen Veri Giriniz.:");
+            Console.Write(">");
+            string veri = Console.ReadLine();
+            if (string.IsNullOrEmpty(veri))
+            {
+                goto basadon;
+            }
+            char[] normal = veri.ToArray();
+            char[] ters = veri.ToArray();
+            Array.Reverse(ters);
+            //araba
+            // a r a b a
+            // a b a r a
+
+            bool palindrom = true;
+            for (int i = 0; i < veri.ToArray().Count(); i++)
+            {
+                if (normal[i] != ters[i])
+                {
+                    palindrom = false;
+                }
+            }
+
+            Console.WriteLine("Palindrom Mu.?" + palindrom);
+            Console.WriteLine(" ");
+
+            goto basadon;
+
+            Console.ReadLine();
+        }
+    }
+}
